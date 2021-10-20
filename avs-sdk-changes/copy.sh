efd2b1b0ae6bcba7df597260dc3d4d3ce977ab15
Rahmadi Trimananda <rtrimana@uci.edu>
2021-10-20 16:52:41 -0700

    Update copy.sh

--- avs-sdk-changes/copy.sh
+++ avs-sdk-changes/copy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo cp avs-changes/main.cpp avs-device-sdk/SampleApp/src/main.cpp
-sudo cp avs-changes/LibcurlHTTP2Request.h avs-device-sdk/AVSCommon/Utils/include/AVSCommon/Utils/LibcurlUtils/LibcurlHTTP2Request.h
-sudo cp avs-changes/LibcurlHTTP2Request.cpp avs-device-sdk/AVSCommon/Utils/src//LibcurlUtils/LibcurlHTTP2Request.cpp
+sudo cp ../avs-changes/main.cpp $1/avs-device-sdk/SampleApp/src/main.cpp
+sudo cp ../avs-changes/LibcurlHTTP2Request.h $1/avs-device-sdk/AVSCommon/Utils/include/AVSCommon/Utils/LibcurlUtils/LibcurlHTTP2Request.h
+sudo cp ../avs-changes/LibcurlHTTP2Request.cpp $1/avs-device-sdk/AVSCommon/Utils/src/LibcurlUtils/LibcurlHTTP2Request.cpp
